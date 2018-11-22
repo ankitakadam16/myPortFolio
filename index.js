3c7ab96f62d89f55cf4119585463bb2e9040a048
cioc <prabhakar.paul12@gmail.com>
2018-11-22 16:09:14 +0530

    abc

---
+++ index.js
@@ -0,0 +1,6 @@
+var app = angular.module('myPortfolio', []);
+app.controller('firstController', function($scope) {
+$scope.name="Ankita Shivanand Kadam"
+$scope.email="ksankita16@gmail.com"
+$scope.phone="8792706346"
+});
